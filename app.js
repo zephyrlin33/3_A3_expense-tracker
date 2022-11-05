5168c7248772e5ba97d6219e7ee7f409deaa1e17
zephyr Lin <zephyrlin33@gmail.com>
2022-11-05 14:48:33 +0800

    add express-session

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express')
-
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 
 const bodyParser = require('body-parser')
@@ -16,15 +16,23 @@ const port = 3000
 // 設定引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.static('public'))// 設定靜態路徑
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+
+// 每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
-// 設定每一筆請求都會透過 methodOverride 進行前置處理
+
+// 每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+
 // 將 request 導入路由器
 app.use(routes)
-
-
 
 
   // start and listen on the Express server
