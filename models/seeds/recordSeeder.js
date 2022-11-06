876f52197a22793dbb9320aa5a8da481f9600f25
zephyr Lin <zephyrlin33@gmail.com>
2022-11-06 23:14:00 +0800

    seed user prob solved

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const Expense = require('../expense.js')
 const User = require('./../user')
 const mongoose = require('mongoose')
@@ -6,11 +10,12 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
 
-const SEED_USER = {
-    name: 'user',
-    email: 'user@example.com',
+const SEED_USER = [{
+    name: 'user1',
+    email: 'user1@example.com',
     password: '12345678'
-}
+}]
+
 
 const SEED_RECORD = [
     { name: '午餐', date: '2022-4-23', number: 60, categoryId: 4 },
@@ -24,17 +29,21 @@ const SEED_RECORD = [
 db.once('open', () => {
     console.log('mongodb connected!')
     console.log('Creating records.....')
-
+    SEED_USER.map(x => {
+        x.password = bcrypt.hashSync(x.password, 10)
+        return x
+    })
     User.create(SEED_USER)
-        .then(user => {
-            const userId = user._id
+        .then((user) => {
+            console.log('新增的user資料',user[0])
+            const userId = user[0]._id
             Promise.all(Array.from({ length: 5 },
                 (_, i) => {
                     SEED_RECORD[i].userId = userId
                 }
             ))
             return Expense.create(SEED_RECORD)
-            
+
         })
         .then(() => {
             console.log('recordSeeder.js is done.')
@@ -42,5 +51,5 @@ db.once('open', () => {
         })
         .catch(err => console.log(err))
 
-    
+
 })
