4beee77146ad4f59e8cbb0fa1ce6fa83f1e37bd7
zephyr Lin <zephyrlin33@gmail.com>
2022-11-06 09:27:19 +0800

    bcrypt

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require ('express')
 const session = require('express-session')
-const exphbs = require('express-handlebars')
 
+const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override') // 載入 method-override
-
+const flash = require('connect-flash')   // 提示錯訊息flash message
 
 const routes = require('./routes')// 引用路由器
 
@@ -35,11 +35,14 @@ app.use(methodOverride('_method'))
 
 
 usePassport(app)
+app.use(flash())  // 掛載套件
 
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const passport = require('passport')// 引用 passport
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 const User = require('../../models/user')
 
@@ -19,6 +20,7 @@ router.post('/login', passport.authenticate('local', {
 
 router.get('/logout', (req, res) => {
     req.logout()
+    req.flash('success_msg', '你已經成功登出。')
     res.redirect('/users/login')
 })
 
@@ -31,29 +33,52 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
     // 取得註冊表單參數
     const { name, email, password, confirmPassword } = req.body
+    const errors = []
+
+    if (!name || !email || !password || !confirmPassword) {
+        errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+
     // 檢查使用者是否已經註冊
     User.findOne({ email }).then(user => {
         // 如果已經註冊：退回原本畫面
         if (user) {
-            console.log('User already exists.')
+            errors.push({ message: '這個 Email 已經註冊過了。' })
+            console.log('這個 Email 已經註冊過了。')
             res.render('register', {
                 name,
                 email,
                 password,
                 confirmPassword
             })
-        } else {
-            // 如果還沒註冊：寫入資料庫
-            return User.create({
+        }
+
+        return bcrypt
+            .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+            .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+            .then(hash => User.create({
                 name,
                 email,
-                password
-            })
-                .then(() => res.redirect('/'))
-                .catch(err => console.log(err))
-        }
+                password: hash // 用雜湊值取代原本的使用者密碼
+            }))
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
     })
         .catch(err => console.log(err))
+
+    if (errors.length) {
+        return res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword
+        })
+    }
+
 })
 
 module.exports = router
